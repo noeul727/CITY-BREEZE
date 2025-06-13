0f0d6d3f267bee5b8faf3bc0d489f9651ef3f683
noeul727 <noeulg82@gmail.com>
2025-06-13 16:26:26 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
     
     breakpoints: {
         0: {
-            slidesPerView: 1.5,
+            slidesPerView: 1.2,
             spaceBetween: 10,
         },
         768: {
